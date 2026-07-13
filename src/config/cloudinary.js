fbe6bc2493ff2ed37d4d14ccd27b86e3dc768d12
suryanshuverma0 <veenasa676@gmail.com>
2026-07-13 17:12:32 +0545

    feat: remove sensitive information logging from Cloudinary configuration

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -9,7 +9,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API Key:", process.env.CLOUDINARY_API_KEY);
-console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
 export default cloudinary;
